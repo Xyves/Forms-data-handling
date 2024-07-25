750dc7c05ba3aeed489137d3c602d2972cd604a7
Xyves <xodrixx@protonmail.com>
2024-07-25 20:48:44 +0200

    First commit

---
+++ routes/usersRouter.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const usersController = require("../controllers/usersController");
+const usersRouter = Router();
+
+usersRouter.get("/create", usersController.usersCreateGet);
+usersRouter.post("/create", usersController.usersCreatePost);
+
+module.exports = usersRouter;
